e9e573767f97e04ca45e021acb766259b197f483
GekJack <75206220+GekJack@users.noreply.github.com>
2024-09-07 11:28:25 +0300

    Add files via upload

---
+++ From/Scripts/script.js
@@ -0,0 +1,32 @@
+const tg = window.Telegram.WebApp;
+tg.expand();
+
+const Submitmouse = () => {
+    const typeProduct = "mouse";
+    const brand = document.getElementById("mouseBrand").value;
+    const mousedpi = document.getElementById("mousedpi").value;
+    const mouse_interface = document.getElementById("mouse-interface").value;
+    const mouseconnection = document.getElementById("mouseconnection").value;
+    const mousesensor = document.getElementById("mousesensor").value;
+    const mousebuttons = document.getElementById("mousebuttons").value;
+    if (!brand || !mousedpi || !mouse_interface || !mouseconnection || !mousesensor || !mousebuttons) {
+        alert("Some fields are null");
+        return;
+    }
+    tg.sendData(JSON.stringify({
+        type_product: typeProduct,
+        brand: brand,
+        mouse_dpi: mousedpi,
+        mouse_interface: mouse_interface,
+        mouse_connection: mouseconnection,
+        mouse_sensor: mousesensor,
+        mouse_buttons: mousebuttons
+    }));
+    tg.close();
+}
+
+
+
+
+const buttonSubmitMouse = document.getElementById("submitMouse");
+buttinSubmitMouse.addEventListener("click", Submitmouse);
