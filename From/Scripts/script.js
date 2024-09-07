a984bed4e0e32bc40dce2393eba4965990829390
GekJack <75206220+GekJack@users.noreply.github.com>
2024-09-07 22:39:45 +0300

    Update script.js

--- From/Scripts/script.js
+++ From/Scripts/script.js
@@ -2,6 +2,7 @@ const tg = window.Telegram.WebApp;
 tg.expand();
 
 const Submitmouse = () => {
+    event.preventDefault();
     const typeProduct = "mouse";
     const brand = document.getElementById("mouseBrand").value;
     const mousedpi = document.getElementById("mousedpi").value;
