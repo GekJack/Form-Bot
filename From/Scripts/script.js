bf1e24ea9b2e0e37bec6a6cd09c1a614a0114993
GekJack <75206220+GekJack@users.noreply.github.com>
2024-09-08 08:46:20 +0300

    Update script.js

--- From/Scripts/script.js
+++ From/Scripts/script.js
@@ -3,22 +3,7 @@ tg.expand();
 const formMouse = document.getElementById("mouseForm");
 const buttonSubmitMouse = document.getElementById("submitMouse");
 
-const checkFields = () => {
-    const brand = document.getElementById("mouseBrand").value;
-    const mousedpi = document.getElementById("mousedpi").value;
-    const mouse_interface = document.getElementById("mouse-interface").value;
-    const mouseconnection = document.getElementById("mouseconnection").value;
-    const mousesensor = document.getElementById("mousesensor").value;
-    const mousebuttons = document.getElementById("mousebuttons").value;
-    const mousePrice = document.getElementById("priceMouse").value;
 
- if (brand && mousedpi && mouse_interface && mouseconnection && mousesensor && mousebuttons && mousePrice) {
-        buttonSubmitMouse.disabled = false; 
-    } else {
-        buttonSubmitMouse.disabled = true; 
-}
-         
- };
 const Submitmouse = () => {
     const typeProduct = "mouse";
     const brand = document.getElementById("mouseBrand").value;
@@ -39,12 +24,6 @@ const Submitmouse = () => {
         mouse_price: mousePrice
     }));
     tg.close();
-}ж
-
-
- formMouse.querySelectorAll('input, select').forEach(element => {
-        element.addEventListener('input', checkFields);
-        element.addEventListener('change', checkFields);
-    });
+}
 
 buttonSubmitMouse.addEventListener("click", Submitmouse);
